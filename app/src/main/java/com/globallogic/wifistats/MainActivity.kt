7e516a94f78ca144677c2277e592f6750a854646
Vasyl Tkachov <vasyl.tkachov@globallogic.com>
2022-08-23 00:36:29 +0200

    add location permission and get ssid

--- app/src/main/java/com/globallogic/wifistats/MainActivity.kt
+++ app/src/main/java/com/globallogic/wifistats/MainActivity.kt
@@ -1,18 +1,40 @@
 package com.globallogic.wifistats
 
+import android.Manifest
 import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.content.IntentSender
+import android.content.pm.PackageManager
+import android.location.LocationManager
 import android.os.Build
 import android.os.Bundle
 import android.provider.Settings
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.ViewModelProvider
 import com.globallogic.wifistats.ui.theme.WifiStatsTheme
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.common.api.ResolvableApiException
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.LocationSettingsRequest
+import com.google.android.gms.location.LocationSettingsStatusCodes
 
 class MainActivity : ComponentActivity() {
 
     lateinit var viewModel: BaseViewModel
+    private val multiplePermissions =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+            if (isGranted) {
+                getWifiStats()
+            }
+            return@registerForActivityResult
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,12 +43,19 @@ class MainActivity : ComponentActivity() {
         setContent {
             WifiStatsTheme {
                 CollectorScreen {
-                    viewModel.collectWifiData()
+                    getWifiStats()
                 }
             }
         }
 
-        Log.d("WIFI", getSystemInfo())
+        Log.d(TAG, getSystemInfo())
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_ENABLE_LOCATION && resultCode == Activity.RESULT_OK) {
+            getWifiStats()
+        }
     }
 
     @SuppressLint("HardwareIds")
@@ -34,6 +63,7 @@ class MainActivity : ComponentActivity() {
         return "Manufacture: ${Build.MANUFACTURER} \n" +
                 "Model: ${Build.MODEL} \n" +
                 "Version Code: ${Build.VERSION.RELEASE} \n" +
+                "Version API: ${Build.VERSION.SDK_INT} \n" +
                 "DeviceID: ${
                     Settings.Secure.getString(
                         contentResolver,
@@ -41,5 +71,82 @@ class MainActivity : ComponentActivity() {
                     )
                 } \n" +
                 "ID: ${Build.ID}"
+    }
+
+    private fun getWifiStats() {
+        if (checkPermission() && isLocationEnabled()) {
+            viewModel.collectWifiData()
+        }
+    }
+
+    private fun checkPermission(): Boolean =
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            multiplePermissions.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+            false
+        } else {
+            true
+        }
+
+
+    private fun isLocationEnabled(): Boolean {
+        val lm = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            lm.isLocationEnabled
+        } else {
+            lm.isProviderEnabled(LocationManager.GPS_PROVIDER) && lm.isProviderEnabled(
+                LocationManager.NETWORK_PROVIDER
+            )
+        }
+    }
+
+    private fun showEnableLocationDialog() {
+        val locationRequest = LocationRequest.create()
+            .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+            .setInterval(2000)
+            .setFastestInterval(1000)
+
+        val settingsBuilder = LocationSettingsRequest.Builder()
+            .addLocationRequest(locationRequest)
+            .setAlwaysShow(true)
+        val result = LocationServices.getSettingsClient(this)
+            .checkLocationSettings(settingsBuilder.build())
+        result.addOnCompleteListener { task ->
+            try {
+                task.getResult(ApiException::class.java)
+            } catch (apiException: ApiException) {
+                when (apiException.statusCode) {
+                    LocationSettingsStatusCodes.RESOLUTION_REQUIRED ->
+                        try {
+                            val resolvableException = apiException as ResolvableApiException
+                            startIntentSenderForResult(
+                                resolvableException.resolution.intentSender,
+                                REQUEST_ENABLE_LOCATION,
+                                null,
+                                0,
+                                0,
+                                0,
+                                null
+                            )
+                        } catch (e: IntentSender.SendIntentException) {
+                            Log.e(TAG, "PendingIntent unable to execute request")
+                        } catch (e: ClassCastException) {
+                            Log.e(TAG, "Location api exception is not resolvable")
+                        }
+                    LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE ->
+                        Log.e(TAG, "Something wrong with locations service")
+                }
+            } catch (exception: Exception) {
+                exception.printStackTrace()
+            }
+        }
+    }
+
+    companion object {
+        private const val TAG = "WIFI"
+        private const val REQUEST_ENABLE_LOCATION = 101
     }
 }
--- app/src/main/java/com/globallogic/wifistats/repository/WifiRepositoryImpl.kt
+++ app/src/main/java/com/globallogic/wifistats/repository/WifiRepositoryImpl.kt
@@ -2,38 +2,78 @@ package com.globallogic.wifistats.repository
 
 import android.content.Context
 import android.net.ConnectivityManager
+import android.net.Network
 import android.net.NetworkCapabilities
+import android.net.NetworkRequest
+import android.net.wifi.WifiInfo
+import android.net.wifi.WifiManager
 import android.os.Build
 import android.util.Log
+import androidx.annotation.RequiresApi
 import javax.inject.Inject
+
 
 class WifiRepositoryImpl @Inject constructor(private val context: Context) : WifiRepository {
 
+    private val connectivityManager =
+        context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+    private val wifiManager =
+        context.applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
+
     override fun collectWifiData() {
-        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-        if (Build.VERSION.SDK_INT >= 29) {
-            val activeNetwork = connectivityManager.activeNetwork ?: return
-            val capabilities = connectivityManager.getNetworkCapabilities(activeNetwork) ?: return
-            val isActiveNetworkValidated = capabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_VALIDATED)
-            Log.d("WIFI", "Network capability validated: $isActiveNetworkValidated")
-            Log.d("WIFI", "Signal strength: ${capabilities.signalStrength}")
-            Log.d("WIFI", "Transport info: ${capabilities.transportInfo.toString()}")
+        val isConnected: Boolean
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            subscribeToNetworkEvents()
+//            val activeNetwork = connectivityManager.activeNetwork ?: return
+//            val capabilities = connectivityManager.getNetworkCapabilities(activeNetwork) ?: return
+//            isConnected = capabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_VALIDATED)
+//            if (isConnected) {
+//                Log.d("WIFI", "Signal strength: ${capabilities.signalStrength}")
+//                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+//                    Log.d("WIFI", "Owner UID: ${capabilities.ownerUid}")
+//                }
+//                val wifiInfo = capabilities.transportInfo as WifiInfo?
+//                wifiInfo?.let { getWifiInfo(wifiInfo) }
+//            }
+        } else {
+            val info = connectivityManager.activeNetworkInfo
+            isConnected = info?.isConnected == true
+            if (isConnected) {
+                val wifiInfo = wifiManager.connectionInfo
+                wifiInfo?.let { getWifiInfo(wifiInfo) }
+            }
         }
     }
 
-    private fun isConnectedToInternet(): Boolean {
-        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-        if (Build.VERSION.SDK_INT >= 29) {
-            val activeNetwork = connectivityManager.activeNetwork ?: return false
-            val capabilities = connectivityManager.getNetworkCapabilities(activeNetwork) ?: return false
-            val isActiveNetworkValidated = capabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_VALIDATED)
-            Log.d("WIFI", "Network capability validated: $isActiveNetworkValidated")
-            return isActiveNetworkValidated
-        } else {
-            val info = connectivityManager.activeNetworkInfo
-            val isConnected = info?.isConnected == true
-            Log.d("WIFI", "Network connected: $isConnected")
-            return isConnected
-        }
+    private fun getWifiInfo(wifiInfo: WifiInfo) {
+        Log.d("WIFI", "ssid: ${wifiInfo.ssid}")
+        Log.d("WIFI", "bssid: ${wifiInfo.bssid}")
+        Log.d("WIFI", "MAC: ${wifiInfo.macAddress}")
+        Log.d("WIFI", "Frequency: ${wifiInfo.frequency}")
+    }
+
+    @RequiresApi(Build.VERSION_CODES.S)
+    private fun subscribeToNetworkEvents() {
+        val request: NetworkRequest =
+            NetworkRequest.Builder()
+                .addTransportType(NetworkCapabilities.TRANSPORT_WIFI)
+                .build()
+        val networkCallback: ConnectivityManager.NetworkCallback =
+            object : ConnectivityManager.NetworkCallback(FLAG_INCLUDE_LOCATION_INFO) {
+                override fun onCapabilitiesChanged(
+                    network: Network,
+                    capabilities: NetworkCapabilities
+                ) {
+                    super.onCapabilitiesChanged(network, capabilities)
+                    val isConnected = capabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_VALIDATED)
+                    Log.d("WIFI", "Has capability: $isConnected")
+                    val wifiInfo = capabilities.transportInfo as WifiInfo?
+                    wifiInfo?.let { getWifiInfo(it) } ?: {
+
+                    }
+                }
+            }
+        connectivityManager.requestNetwork(request, networkCallback)
+        connectivityManager.registerNetworkCallback(request, networkCallback)
     }
 }
